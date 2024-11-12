acb8e5430d5afb92f6eeffeba46bf64c87a4dad3
GrinevArtem <artemgrinyov90@gmail.com>
2024-11-12 11:35:38 +0300

    feat: добавлены базовые компоненты layout
    - Добавлен компонент Navbar
    - Добавлен компонент MainContainer
    - Добавлен компонент Footer
    - Изменено: layouts/default.vue

---
+++ components/layout/MainContainer.vue
@@ -0,0 +1,35 @@
+<script setup></script>
+<template>
+  <main class="Container max-w-screen-xl mx-auto">
+    <slot />
+  </main>
+</template>
+
+<style lang="css">
+.Container {
+  width: 100%;
+  height: 100dvh;
+  padding: 0 10px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+@media (min-width: 768px) and (max-width: 1023px) {
+  .Container {
+    width: 760px;
+    padding: 0 30px;
+  }
+}
+
+@media (min-width: 1024px) and (max-width: 1279px) {
+  .Container {
+    width: 1000px;
+  }
+}
+
+@media (min-width: 1280px) {
+  .Container {
+    width: 1260px;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,6 @@
 <script setup></script>
 <template>
-  <BaseNavbar></BaseNavbar>
   <BaseContainer>
     <slot></slot>
   </BaseContainer>
-  <BaseFooter></BaseFooter>
 </template>
