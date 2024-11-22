fe2047ccd81f7d8370e0209f469adbf1557f2a9a
GrinevArtem <artemgrinyov90@gmail.com>
2024-11-22 21:00:43 +0300

    feat(create-recipe): Добавленно поле выбора продукта
    - Добавлено поле выбора продукта через модальное окно и вызов API
    - Реализован автоматический вывод добавленых продуктов

--- pages/admin/create/recipe.vue
+++ pages/admin/create/recipe.vue
@@ -16,82 +16,98 @@
     <UInput class="mb-4" v-model.number="activeTime" type="number" />
 
     <h3>Подготовка</h3>
-    <UInput class="mb-4" v-model.number="preparationTime" type="number" />
+    <UInput class="mb-4" v-model="preliminaryPreparation" />
 
     <h3>Сложность</h3>
     <UInputMenu v-model="selected" :options="difficulty" />
 
+    <!-- ИНГРИДИЕНТЫ -->
+
     <UDivider class="py-4" label="ИНГРИДИЕНТЫ" size="sm" />
 
-    <div
-      v-for="(input, index) in inputsIngridient"
-      :key="'ingredient-' + index"
-      class="flex justify-between mb-4"
-    >
-      <UInput
-        class="pr-3 w-full"
-        v-model="input.title"
-        placeholder="Ингредиент"
-      />
-      <UInput
-        class="pr-3 w-32"
-        v-model.number="input.count"
-        type="number"
-        size="sm"
-        placeholder="Количество"
-      />
-      <UInputMenu v-model="input.amount" :options="amountOptions" />
-      <UButton
-        icon="mdi:delete"
-        @click.prevent="removeInputField(index)"
-        class="ml-2 mr-3"
-      />
+    <div>
+      <div v-if="ingredients.length > 0" class="mb-4">
+        <h3>Добавленные ингредиенты:</h3>
+        <ul>
+          <li v-for="(ingredient, index) in ingredients" :key="index">
+            {{ ingredient.title }} - {{ ingredient.count }}
+            {{ ingredient.amount }}
+          </li>
+        </ul>
+      </div>
+      <div class="flex justify-between mb-4">
+        <UInput
+          class="pr-3 w-full"
+          v-model="newIngredient.title"
+          placeholder="Ингредиент"
+        />
+        <UInput
+          class="pr-3"
+          v-model="newIngredient.count"
+          type="number"
+          size="sm"
+          placeholder="Количество"
+        />
+        <UInputMenu
+          class="w-28 mr-3"
+          v-model="newIngredient.amount"
+          :options="amountOptions"
+        />
+        <UButton class="mr-3" label="Добавить продукт" />
+        <UButton label="Добавить ингредиент" @click.prevent="addIngredient" />
+      </div>
     </div>
 
-    <UButton label="Добавить ингредиент" @click.prevent="addInputField" />
+    <!-- ПРОДУКТЫ -->
 
     <UDivider class="py-4" label="ПРОДУКТЫ" size="sm" />
-
-    <div
-      v-for="(input, index) in inputsProducts"
-      :key="'product-' + index"
-      class="flex justify-between mb-4"
-    >
-      <UInput
-        class="pr-3 w-full"
-        v-model="value"
-        placeholder="Выбрать продукт"
-        @click="isOpen = true"
-      />
-      <UModal v-model="isOpen">
-        <UCommandPalette
-          :groups="groups"
-          :autoselect="false"
-          @update:model-value="onProductSelect"
+    <div>
+      <div v-if="products.length > 0" class="mb-4">
+        <h3>Добавленные продукты:</h3>
+        <ul>
+          <li v-for="(product, index) in products" :key="index">
+            {{ product.title }} - {{ product.count }}
+            {{ product.amount }}
+          </li>
+        </ul>
+      </div>
+      <div class="flex justify-between mb-4">
+        <UInput
+          class="pr-3 w-full"
+          v-model="newProducts.title"
+          placeholder="Выбрать продукт"
+          @click="isOpen = true"
         />
-      </UModal>
-
-      <UInput
-        class="pr-3 w-32"
-        v-model.number="input.count"
-        type="number"
-        size="sm"
-        placeholder="Количество"
-      />
-      <UInputMenu v-model="input.amount" :options="amountOptions" />
-      <UButton
-        icon="mdi:delete"
-        @click.prevent="removeInputProductField(index)"
-        class="ml-2 mr-3"
-      />
+        <UModal v-model="isOpen">
+          <UCommandPalette
+            :groups="groups"
+            :autoselect="false"
+            @update:model-value="onProductSelect"
+          />
+        </UModal>
+        <UBadge class="mr-3" label="Badge" />
+        <UBadge class="mr-3" label="Badge" />
+        <UInput
+          class="pr-3"
+          v-model="newProducts.count"
+          type="number"
+          size="sm"
+          placeholder="Количество"
+        />
+        <UInputMenu
+          class="w-28 mr-3"
+          v-model="newProducts.amount"
+          :options="amountOptions"
+        />
+        <UButton class="mr-3" label="Альтернатива" />
+        <UButton label="Добавить продукт" @click.prevent="addProduct" />
+      </div>
     </div>
-
-    <UButton label="Добавить продукт" @click.prevent="addInputProductField" />
   </div>
 </template>
 
 <script setup>
-import { ref, toRaw } from "vue";
+import { ref } from "vue";
 
 definePageMeta({
   layout: "content",
@@ -101,19 +117,35 @@ const title = ref("");
 const description = ref("");
 const cookingTime = ref(0);
 const activeTime = ref(0);
-const preparationTime = ref(0);
+const preliminaryPreparation = ref("");
 const difficulty = ["Легкий", "Средний", "Сложный"];
 const selected = ref(difficulty[0]);
-const inputsIngridient = ref([{ title: "", count: "", amount: "кг." }]);
 const amountOptions = ["кг.", "гр.", "л.", "мл.", "шт."];
-
-function addInputField() {
-  inputsIngridient.value.push({ title: "", count: 0, amount: "кг." });
+// ИНГРИДИЕНТЫ
+const ingredients = ref([]);
+const newIngredient = ref({ title: "", count: 0, amount: "кг." });
+function addIngredient() {
+  if (newIngredient.value.title && newIngredient.value.count) {
+    ingredients.value.push({ ...newIngredient.value });
+    newIngredient.value = { title: "", count: "", amount: "кг." };
+  }
+}
+// ПРОДУКТЫ
+const products = ref([]);
+const newProducts = ref({ id: "", title: "", count: 0, amount: "кг." });
+function addProduct() {
+  if (newProducts.value.title && newProducts.value.count) {
+    products.value.push({ ...newProducts.value });
+    newProducts.value = { title: "", count: "", amount: "кг." };
+  }
+}
+function onProductSelect(selected) {
+  console.log("Selected:", selected);
+  console.log(selected.label);
+  newProducts.value.title = selected.label;
+  isOpen.value = false;
 }
 
-function removeInputField(index) {
-  inputsIngridient.value.splice(index, 1);
-}
 const groups = [
   {
     key: "users",
@@ -138,22 +170,6 @@ const groups = [
 ];
 
 const isOpen = ref(false);
-let value = ref("");
-function onProductSelect(selected) {
-  console.log("Selected:", selected);
-  console.log(selected.label);
-  value = selected.label;
-  isOpen.value = false;
-}
-const inputsProducts = ref([{ label: "", count: 0, amount: "кг." }]);
-
-function addInputProductField() {
-  inputsProducts.value.push({ label: "", count: 0, amount: "кг." });
-}
-
-function removeInputProductField(index) {
-  inputsProducts.value.splice(index, 1);
-}
 </script>
 
 <style scoped>
